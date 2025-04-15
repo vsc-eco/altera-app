5e0184e0de4382d16989ca5ad543256fc5b94efd
zphrs <z@zephiris.dev>
2025-04-14 20:49:58 -0700

    added back move money dropdown

--- src/lib/Topbar/Topbar.svelte
+++ src/lib/Topbar/Topbar.svelte
@@ -37,7 +37,7 @@
 	<button class="transparent-icon" onclick={onMenuToggle}> <MenuIcon></MenuIcon> </button>
 	<ComboSearch />
 
-	<!-- <Menu
+	<Menu
 		items={actions.map((a) => {
 			return {
 				label: a.label,
@@ -50,7 +50,7 @@
 			let action = actions.find((v) => v.label == e.value)!;
 			goto(action.href);
 		}}
-	/> -->
+	/>
 	<!-- <Notifications /> -->
 	{#snippet prefs(name: string)}
 		{name}

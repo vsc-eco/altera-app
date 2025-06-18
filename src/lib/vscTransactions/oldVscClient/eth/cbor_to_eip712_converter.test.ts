5e3a0a74fe0a64f02225d600acd2b55b0ad7baa1
Geoffrey Casper <gcasper42@gmail.com>
2025-06-18 15:20:31 -0400

    eip712: allow for dynamic array types

--- src/lib/vscTransactions/oldVscClient/eth/cbor_to_eip712_converter.test.ts
+++ src/lib/vscTransactions/oldVscClient/eth/cbor_to_eip712_converter.test.ts
@@ -1,249 +1,257 @@
-import { describe, it, expect } from '@jest/globals'
-import { convertCBORToEIP712TypedData } from './cbor_to_eip712_converter'
-import { encodePayload } from 'dag-jose-utils'
+import { describe, it, expect } from 'vitest';
+import { convertCBORToEIP712TypedData } from './cbor_to_eip712_converter';
+import { encodePayload } from 'dag-jose-utils';
 
-import { hashDomain, hashTypedData } from 'viem'
+import { hashDomain, hashTypedData } from 'viem';
 
-const intents: unknown[] = []
+const intents: unknown[] = [];
 
 describe('eth msg', () => {
-  it('domain hash', () => {
-    expect(
-      hashDomain({
-        domain: {
-          name: 'vsc.network',
-        },
-        types: {
-          EIP712Domain: [
-            {
-              name: 'name',
-              type: 'string',
-            },
-          ],
-        },
-      }),
-    ).toBe('0xb364cbb4ec1c3d3d438ef95f01322f22b04280d481abaa8cd6c7b5c7108f1a7e')
-  })
+	it('domain hash', () => {
+		expect(
+			hashDomain({
+				domain: {
+					name: 'vsc.network'
+				},
+				types: {
+					EIP712Domain: [
+						{
+							name: 'name',
+							type: 'string'
+						}
+					]
+				}
+			})
+		).toBe('0xb364cbb4ec1c3d3d438ef95f01322f22b04280d481abaa8cd6c7b5c7108f1a7e');
+	});
 
-  it('matches conversion', async () => {
-    const res = (
-      await encodePayload({
-        __t: 'vsc-tx',
-        __v: '0.2',
-        headers: {
-          intents: intents, // TODO should this be here
-          nonce: 13,
-          required_auths: [
-            'did:pkh:eip155:1:0x88EBB64C264AFf10141149F9770F8D644C9D86C5',
-          ],
-          type: 1,
-        },
-        tx: {
-          op: 'transfer',
-          payload: {
-            amount: 1,
-            from: 'did:pkh:eip155:1:0x88EBB64C264AFf10141149F9770F8D644C9D86C5',
-            tk: 'HIVE',
-            to: 'did:pkh:eip155:1:0x88EBB64C264AFf10141149F9770F8D644C9D86C5',
-          },
-        },
-      })
-    ).linkedBlock
-    expect(
-      convertCBORToEIP712TypedData('vsc.network', res, 'tx_container_v0'),
-    ).toStrictEqual({
-      EIP712Domain: [
-        {
-          name: 'name',
-          type: 'string',
-        },
-      ],
-      domain: {
-        name: 'vsc.network',
-      },
-      primaryType: 'tx_container_v0',
-      message: {
-        __t: 'vsc-tx',
-        __v: '0.2',
-        headers: {
-          intents: intents, // TODO should this be here
-          nonce: 13,
-          required_auths: [
-            'did:pkh:eip155:1:0x88EBB64C264AFf10141149F9770F8D644C9D86C5',
-          ],
-          type: 1,
-        },
-        tx: {
-          op: 'transfer',
-          payload: {
-            amount: 1,
-            from: 'did:pkh:eip155:1:0x88EBB64C264AFf10141149F9770F8D644C9D86C5',
-            tk: 'HIVE',
-            to: 'did:pkh:eip155:1:0x88EBB64C264AFf10141149F9770F8D644C9D86C5',
-          },
-        },
-      },
-      types: {
-        tx_container_v0: [
-          {
-            name: 'tx',
-            type: 'tx_container_v0.tx',
-          },
-          {
-            name: '__t',
-            type: 'string',
-          },
-          {
-            name: '__v',
-            type: 'string',
-          },
-          {
-            name: 'headers',
-            type: 'tx_container_v0.headers',
-          },
-        ],
-        'tx_container_v0.headers': [
-          {
-            name: 'type',
-            type: 'uint256',
-          },
-          {
-            name: 'nonce',
-            type: 'uint256',
-          },
-          {
-            name: 'intents',
-            type: 'undefined[]',
-          },
-          {
-            name: 'required_auths',
-            type: 'string[]',
-          },
-        ],
-        'tx_container_v0.tx': [
-          {
-            name: 'op',
-            type: 'string',
-          },
-          {
-            name: 'payload',
-            type: 'tx_container_v0.tx.payload',
-          },
-        ],
-        'tx_container_v0.tx.payload': [
-          {
-            name: 'tk',
-            type: 'string',
-          },
-          {
-            name: 'to',
-            type: 'string',
-          },
-          {
-            name: 'from',
-            type: 'string',
-          },
-          {
-            name: 'amount',
-            type: 'uint256',
-          },
-        ],
-      },
-    })
-  })
+	it('matches conversion', async () => {
+		const res = (
+			await encodePayload({
+				__t: 'vsc-tx',
+				__v: '0.2',
+				headers: {
+					net_id: 'vsc-mainnet',
+					nonce: 0 as any,
+					rc_limit: 0 as any,
+					required_auths: ['did:pkh:eip155:1:0x31160b3fF46E7406D7B28af9128b0407e05C376e']
+				},
+				tx: [
+					{
+						payload:
+							'{"amount":"0.001","asset":"hbd","from":"did:pkh:eip155:1:0x31160b3fF46E7406D7B28af9128b0407e05C376e","to":"hive:vsc.account"}',
+						type: 'transfer'
+					},
+					{
+						payload:
+							'{"amount":"0.001","asset":"hbd","from":"did:pkh:eip155:1:0x31160b3fF46E7406D7B28af9128b0407e05C376e","to":"hive:vsc.account"}',
+						type: 'transfer'
+					}
+				]
+			})
+		).linkedBlock;
+		expect(convertCBORToEIP712TypedData('vsc.network', res, 'tx_container_v0')).toStrictEqual({
+			EIP712Domain: [
+				{
+					name: 'name',
+					type: 'string'
+				}
+			],
+			domain: {
+				name: 'vsc.network'
+			},
+			primaryType: 'tx_container_v0',
+			message: {
+				__t: 'vsc-tx',
+				__v: '0.2',
+				headers: {
+					net_id: 'vsc-mainnet',
+					nonce: 0 as any,
+					rc_limit: 0 as any,
+					required_auths: ['did:pkh:eip155:1:0x31160b3fF46E7406D7B28af9128b0407e05C376e']
+				},
+				tx: {
+					_0_: {
+						payload:
+							'{"amount":"0.001","asset":"hbd","from":"did:pkh:eip155:1:0x31160b3fF46E7406D7B28af9128b0407e05C376e","to":"hive:vsc.account"}',
+						type: 'transfer'
+					},
+					_1_: {
+						payload:
+							'{"amount":"0.001","asset":"hbd","from":"did:pkh:eip155:1:0x31160b3fF46E7406D7B28af9128b0407e05C376e","to":"hive:vsc.account"}',
+						type: 'transfer'
+					}
+				}
+			},
+			types: {
+				tx_container_v0: [
+					{
+						name: 'tx',
+						type: 'tx_container_v0_tx'
+					},
+					{
+						name: '__t',
+						type: 'string'
+					},
+					{
+						name: '__v',
+						type: 'string'
+					},
+					{
+						name: 'headers',
+						type: 'tx_container_v0_headers'
+					}
+				],
+				tx_container_v0_headers: [
+					{
+						name: 'nonce',
+						type: 'uint256'
+					},
+					{
+						name: 'net_id',
+						type: 'string'
+					},
+					{
+						name: 'rc_limit',
+						type: 'uint256'
+					},
+					{
+						name: 'required_auths',
+						type: 'string[]'
+					}
+				],
+				tx_container_v0_tx: [
+					{
+						name: '_0_',
+						type: 'tx_container_v0_tx__0_'
+					},
+					{
+						name: '_1_',
+						type: 'tx_container_v0_tx__1_'
+					}
+				],
+				tx_container_v0_tx__0_: [
+					{
+						name: 'type',
+						type: 'string'
+					},
+					{
+						name: 'payload',
+						type: 'string'
+					}
+				],
+				tx_container_v0_tx__1_: [
+					{
+						name: 'type',
+						type: 'string'
+					},
+					{
+						name: 'payload',
+						type: 'string'
+					}
+				]
+			}
+		});
+	});
 
-  it('full hash', () => {
-    expect(
-      hashTypedData({
-        primaryType: 'tx_container_v0',
-        message: {
-          __t: 'vsc-tx',
-          __v: '0.2',
-          headers: {
-            intents: [] as any, // TODO should this be here
-            nonce: 13n,
-            required_auths: [
-              'did:pkh:eip155:1:0x88EBB64C264AFf10141149F9770F8D644C9D86C5',
-            ],
-            type: 1n,
-          },
-          tx: {
-            op: 'transfer',
-            payload: {
-              amount: 1n,
-              from: 'did:pkh:eip155:1:0x88EBB64C264AFf10141149F9770F8D644C9D86C5',
-              tk: 'HIVE',
-              to: 'did:pkh:eip155:1:0x88EBB64C264AFf10141149F9770F8D644C9D86C5',
-            },
-          },
-        },
-        types: {
-          tx_container_v0: [
-            {
-              name: 'tx',
-              type: 'tx_container_v0.tx',
-            },
-            {
-              name: '__t',
-              type: 'string',
-            },
-            {
-              name: '__v',
-              type: 'string',
-            },
-            {
-              name: 'headers',
-              type: 'tx_container_v0.headers',
-            },
-          ],
-          'tx_container_v0.headers': [
-            {
-              name: 'type',
-              type: 'uint256',
-            },
-            {
-              name: 'nonce',
-              type: 'uint256',
-            },
-            {
-              name: 'intents',
-              type: 'undefined[]',
-            },
-            {
-              name: 'required_auths',
-              type: 'string[]',
-            },
-          ],
-          'tx_container_v0.tx': [
-            {
-              name: 'op',
-              type: 'string',
-            },
-            {
-              name: 'payload',
-              type: 'tx_container_v0.tx.payload',
-            },
-          ],
-          'tx_container_v0.tx.payload': [
-            {
-              name: 'tk',
-              type: 'string',
-            },
-            {
-              name: 'to',
-              type: 'string',
-            },
-            {
-              name: 'from',
-              type: 'string',
-            },
-            {
-              name: 'amount',
-              type: 'uint256',
-            },
-          ],
-        },
-      }),
-    ).toBe('0x793380cf3e30423e2dfb965c8a1f81d7c68055fd1b4d1a953f10a54ff6418f88')
-  })
-})
+	it('full hash', () => {
+		expect(
+			hashTypedData({
+				types: {
+					tx_container_v0: [
+						{
+							name: 'tx',
+							type: 'tx_container_v0_tx'
+						},
+						{
+							name: '__t',
+							type: 'string'
+						},
+						{
+							name: '__v',
+							type: 'string'
+						},
+						{
+							name: 'headers',
+							type: 'tx_container_v0_headers'
+						}
+					],
+					tx_container_v0_headers: [
+						{
+							name: 'nonce',
+							type: 'uint256'
+						},
+						{
+							name: 'net_id',
+							type: 'string'
+						},
+						{
+							name: 'rc_limit',
+							type: 'uint256'
+						},
+						{
+							name: 'required_auths',
+							type: 'string[]'
+						}
+					],
+					tx_container_v0_tx: [
+						{
+							name: '_0_',
+							type: 'tx_container_v0_tx__0_'
+						},
+						{
+							name: '_1_',
+							type: 'tx_container_v0_tx__1_'
+						}
+					],
+					tx_container_v0_tx__0_: [
+						{
+							name: 'type',
+							type: 'string'
+						},
+						{
+							name: 'payload',
+							type: 'string'
+						}
+					],
+					tx_container_v0_tx__1_: [
+						{
+							name: 'type',
+							type: 'string'
+						},
+						{
+							name: 'payload',
+							type: 'string'
+						}
+					]
+				},
+				primaryType: 'tx_container_v0',
+				domain: {
+					name: 'vsc.network'
+				},
+				message: {
+					__t: 'vsc-tx',
+					__v: '0.2',
+					headers: {
+						net_id: 'vsc-mainnet',
+						nonce: 0 as any,
+						rc_limit: 0 as any,
+						required_auths: ['did:pkh:eip155:1:0x31160b3fF46E7406D7B28af9128b0407e05C376e']
+					},
+					tx: {
+						_0_: {
+							payload:
+								'{"amount":"0.001","asset":"hbd","from":"did:pkh:eip155:1:0x31160b3fF46E7406D7B28af9128b0407e05C376e","to":"hive:vsc.account"}',
+							type: 'transfer'
+						},
+						_1_: {
+							payload:
+								'{"amount":"0.001","asset":"hbd","from":"did:pkh:eip155:1:0x31160b3fF46E7406D7B28af9128b0407e05C376e","to":"hive:vsc.account"}',
+							type: 'transfer'
+						}
+					}
+				}
+			})
+		).toBe('0x22db3e32a50eac722e9ee18fbedf6a049885b379bfe887a0792e69efe660b58f');
+	});
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { svelteTesting } from '@testing-library/svelte/vite';
 import houdini from 'houdini/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
@@ -5,7 +6,30 @@ import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
 	plugins: [houdini(), sveltekit(), mkcert()],
-	optimizeDeps: {
-		exclude: ['@urql/svelte']
+	optimizeDeps: { exclude: ['@urql/svelte'] },
+	test: {
+		projects: [
+			{
+				extends: './vite.config.ts',
+				plugins: [svelteTesting()],
+				test: {
+					name: 'client',
+					environment: 'jsdom',
+					clearMocks: true,
+					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+					exclude: ['src/lib/server/**'],
+					setupFiles: ['./vitest-setup-client.ts']
+				}
+			},
+			{
+				extends: './vite.config.ts',
+				test: {
+					name: 'server',
+					environment: 'node',
+					include: ['src/**/*.{test,spec}.{js,ts}'],
+					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+				}
+			}
+		]
 	}
 });

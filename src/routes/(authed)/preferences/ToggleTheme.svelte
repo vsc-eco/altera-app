448f7980acab6d6b579468a3368f2c3ff5a1774d
Milo Ridenour <mirideno@ucsc.edu>
2025-06-18 09:31:22 -0700

    added dark mode toggle

---
+++ src/routes/(authed)/preferences/ToggleTheme.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Select from '$lib/zag/Select.svelte';
+    import { getInitialTheme, THEMES, theme } from '$lib/theme';
+</script>
+<h2>Theme</h2>
+<Select
+    items={Object.values(THEMES)}
+    initial={THEMES[getInitialTheme()].label}
+    onValueChange={(v) => {
+        theme.set(v.items[0]);
+    }}
+/>

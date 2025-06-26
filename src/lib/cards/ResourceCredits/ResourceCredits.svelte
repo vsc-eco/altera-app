23d8e7db496d8752d1a5bbc77c70a7358b57c9eb
Milo Ridenour <mirideno@ucsc.edu>
2025-06-25 17:37:20 -0700

    vsc resource credits working, no tooltip or ETA recharge

--- src/lib/cards/ResourceCredits/ResourceCredits.svelte
+++ src/lib/cards/ResourceCredits/ResourceCredits.svelte
@@ -2,13 +2,19 @@
     import Card from "../Card.svelte";
     import Progress from "$lib/zag/Progress.svelte";
     import { GetAccountRCStore } from "$houdini";
-
-    let api = $derived(new GetAccountRCStore());
-    let rcs = $derived($api.data?.getAccountRC ?? 0);
-
+    import { accountBalance } from "$lib/balances";
 </script>
 
 <Card>
-    <h2>Resource Credits</h2>
-    <Progress boundaries={{min: 0, max: 100}} currentValue={50}/>
+    <div class="rc-wrapper">
+        <h2>Resource Credits</h2>
+        <h5>VSC Resource Credits</h5>
+        <Progress boundaries={{min: 0, max: $accountBalance.hbd}} currentValue={$accountBalance.resource_credits}/>
+    </div>
 </Card>
+
+<style lang="scss">
+    .rc-wrapper {
+        margin: 1rem;
+    }
+</style>
--- src/lib/zag/Progress.svelte
+++ src/lib/zag/Progress.svelte
@@ -2,7 +2,6 @@
 	import * as progress from '@zag-js/progress';
 	import { normalizeProps, useMachine } from '@zag-js/svelte';
 	import { getUniqueId } from './idgen';
-	import { untrack } from 'svelte';
 	type Props = {
 		boundaries: { min: number; max: number };
 		currentValue: number;
@@ -24,12 +23,28 @@
 </script>
 
 <div {...api.getRootProps()}>
-	<div {...api.getLabelProps()}>VSC Credits</div>
+	<div {...api.getLabelProps()}>
+		<span class="coin-amt current">
+			{currentValue * (10 ** -3)}
+		</span>
+		<span class="coin-amt max">
+			/ {boundaries.max * (10 ** -3)}
+		</span>
+	</div>
 	<div {...api.getTrackProps()}>
 		<div {...api.getRangeProps()}></div>
 	</div>
 	<div {...api.getValueTextProps()}>
-		{Math.round((displayValue / boundaries.max) * 100)}%
+		<span class="legend">
+			<span class="legend-label">Available</span>
+			<span class="legend-icon"> </span>
+		</span>
+		<span class="percentage">
+			{#if currentValue < displayValue}
+				&lt;
+			{/if}
+			{displayValue ? Math.round((displayValue / boundaries.max) * 100) : 0}%
+		</span>
 	</div>
 </div>
 
@@ -37,21 +52,37 @@
     // [data-scope="progress"][data-part="root"] {
         
     // }
-	// [data-scope="progress"][data-part="label"] {
-	
-	// }
+	[data-scope="progress"][data-part="label"] {
+		display: flex;
+		flex-direction: row;
+		width: 100%;
+		align-items: baseline;
+		.coin-amt {
+			font-family: 'Noto Sans Mono Variable', monospace;
+			font-weight: 400;
+		}
+		.coin-amt.current {
+			font-size: var(--text-2xl);
+		}
+		.coin-amt.max {
+			width: 100%;
+			text-align: right;
+			height: fit-content;
+		}
+	}
 	[data-scope='progress'][data-part='track'] {
 		height: 0.5rem;
-		background-color: var(--neutral-fg-accent);
+		background-color: var(--neutral-bg-accent-shifted);
 		border-radius: 0.5rem;
 		overflow: hidden;
 		position: relative;
-        margin: 0.5rem 0;
+        margin-top: 1rem;
+		margin-bottom: 0.5rem;
 	}
 
 	[data-scope='progress'][data-part='range'] {
 		height: 100%;
-		background-color: var(--primary-fg-mid);
+		background-color: var(--primary-bg-mid);
 		border-radius: 0.5rem;
 		transition: width 0.3s ease;
 		position: absolute;
@@ -60,8 +91,31 @@
 	}
 
     [data-scope="progress"][data-part="value-text"] {
+		display: flex;
+		flex-direction: row;
         color: var(--neutral-fg-mid);
 		font-size: var(--text-sm);
-        text-align: right;
+		.percentage {
+			width: 100%;
+			text-align: right;
+		}
+		.legend {
+			display: grid;
+			max-width: max-content;
+			grid-template-areas:
+				'legend-label	legend-icon';
+			align-items: center;
+			gap: 0.25rem;
+		}
+		.legend-label {
+			grid-area: legend-label;
+		}
+		.legend-icon {
+			grid-area: legend-icon;
+			width: 0.5rem;
+			height: 0.5rem;
+			border-radius: 0.5rem;
+			background-color: var(--primary-bg-mid);
+		}
     }
 </style>

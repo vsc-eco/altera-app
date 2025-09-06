0998684e8aa4572f43797565966c43bc4e5f108f
Milo Ridenour <milo.hpr@gmail.com>
2025-09-05 18:11:07 -0700

    removed VERCEL_URL public variable

--- src/lib/auth/url.ts
+++ src/lib/auth/url.ts
@@ -1,4 +1,3 @@
 import { browser } from '$app/environment';
-import { PUBLIC_VERCEL_URL } from '$env/static/public';
 
-export const DOMAIN = browser ? window.location.host : PUBLIC_VERCEL_URL;
+export const DOMAIN = browser ? window.location.host : '';

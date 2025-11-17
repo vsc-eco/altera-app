1826f583a6bc26bab89861e2a4d3e37a0b702053
sagar <sag333ar@gmail.com>
2025-11-18 05:16:36 +0530

    removed bitcoin withdraw option as suggested by milo in the pull request

--- src/lib/sendswap/stages/withdraw/WithdrawOptions.svelte
+++ src/lib/sendswap/stages/withdraw/WithdrawOptions.svelte
@@ -2,14 +2,12 @@
 	import ClickableCard from '$lib/cards/ClickableCard.svelte';
 	import ImageIconRenderer from '$lib/components/ImageIconRenderer.svelte';
 	import { ArrowLeft, ChevronRight } from '@lucide/svelte';
-	import swapOptions, { Coin, Network, TransferMethod } from '../../utils/sendOptions';
+	import { Network, TransferMethod } from '../../utils/sendOptions';
 	import { SendTxDetails } from '../../utils/sendUtils';
 	import HiveMainnetWithdraw from './HiveMainnetWithdraw.svelte';
-	import LightningWithdraw from './LightningWithdraw.svelte';
 	import { untrack, type ComponentProps } from 'svelte';
 	import PillButton from '$lib/PillButton.svelte';
 	import NavButtons, { type NavButton } from '$lib/sendswap/components/NavButtons.svelte';
-	import { getAuth } from '$lib/auth/store';
 
 	let {
 		editStage,
@@ -21,34 +19,15 @@
 		customButtons: ComponentProps<typeof NavButtons>['buttons'] | undefined;
 	} = $props();
 
-	const auth = $derived(getAuth()());
-
-	let lightningOpen = $state(false);
 	let hiveMainnetOpen = $state(false);
 
-	let toggleLightning: (open?: boolean) => void = (open = false) => {
-		lightningOpen = open;
-	};
 	let toggleHiveMainnet: (open?: boolean) => void = (open = false) => {
 		hiveMainnetOpen = open;
 	};
 
 	$effect(() => {
-		if (!lightningOpen) return;
-		untrack(() => {
-			toggleHiveMainnet(false);
-			$SendTxDetails.method = TransferMethod.lightningTransfer;
-			$SendTxDetails.toNetwork = Network.lightning;
-			$SendTxDetails.toCoin = swapOptions.to.coins.find(
-				(coinOpt) => coinOpt.coin.value === Coin.btc.value
-			);
-		});
-	});
-
-	$effect(() => {
 		if (!hiveMainnetOpen) return;
 		untrack(() => {
-			toggleLightning(false);
 			$SendTxDetails.method = TransferMethod.magiTransfer;
 			$SendTxDetails.toNetwork = Network.hiveMainnet;
 			$SendTxDetails.toCoin = $SendTxDetails.fromCoin;
@@ -56,20 +35,12 @@
 	});
 
 	$effect(() => {
-		onHomePage = lightningOpen || hiveMainnetOpen;
+		onHomePage = hiveMainnetOpen;
 	});
 </script>
 
 <div class="withdraw-internal-wrapper">
-	{#if lightningOpen}
-		<PillButton onclick={() => toggleLightning()} styleType="icon-subtle">
-			<ArrowLeft size={32} />
-		</PillButton>
-		<h2>Lightning Withdraw</h2>
-		<div class="withdraw-content">
-			<LightningWithdraw {editStage} open={lightningOpen} />
-		</div>
-	{:else if hiveMainnetOpen}
+	{#if hiveMainnetOpen}
 		<PillButton onclick={() => toggleHiveMainnet()} styleType="icon-subtle">
 			<ArrowLeft size={32} />
 		</PillButton>
@@ -80,21 +51,6 @@
 	{:else}
 		<h2>Withdraw</h2>
 		<div class="types-wrapper">
-			<div class="lightning">
-				<ClickableCard onclick={() => toggleLightning(true)}>
-					<div class="type-header">
-						<ImageIconRenderer
-							icon={Network.lightning.icon}
-							alt={Network.lightning.label}
-							size={40}
-						/>
-						<span>Lightning Transfer</span>
-						<div class="chevron">
-							<ChevronRight />
-						</div>
-					</div>
-				</ClickableCard>
-			</div>
 			<div class="hive-mainnet">
 				<ClickableCard onclick={() => toggleHiveMainnet(true)}>
 					<div class="type-header">
